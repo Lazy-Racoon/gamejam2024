ba5d1e0c52990e038646b17d9cd4f831274e65ed
Ariel Vernaza <ariel@farms2friends.com>
2024-01-27 01:14:10 -0500

    Monkey movement test

---
+++ Assets/Scripts/MonkeyController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class MonkeyController : MonoBehaviour
+{
+    public float score = 0f;
+    public float speed = 10f;
+    private float x,y,z;
+    private Rigidbody rb;
+    private void Awake(){
+      rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = gameObject.transform.position.x;
+        y = gameObject.transform.position.y;
+        z = gameObject.transform.position.z;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal")* speed, 0, Input.GetAxis("Vertical") * speed);
+        if(Input.GetKey(KeyCode.Space)){
+            Debug.Log("Tira kaka en"+ x+" "+y+" "+z);
+         }
+    }
+}
