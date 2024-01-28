62fd46c3218deecc1a7bec9813792eda30ab6af5
Ariel Vernaza <ariel@farms2friends.com>
2024-01-28 09:37:44 -0500

    Board (#14)
    * test board
    
    * Cleaning
    
    * Update
    
    * Display
    
    ---------
    
    Co-authored-by: Ariel Vernaza <arielvernaza@transfix.io>

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CarterGames.Assets.LeaderboardManager.Demo
+{
+    public class BoardManager : MonoBehaviour
+    {
+        [SerializeField] private InputField playerName;
+        
+        
+        public void AddToBoard()
+        {
+            if (playerName.text.Length <= 0 )
+            {
+                Debug.Log($"<color=D6BA64><b>Leaderboard Manager Example</b> | Either the name or score fields were blank, please ensure the fields are filled before using this option.</color>");
+                return;
+            }
+            
+            LeaderboardManager.AddEntryToBoard("tirakaka", playerName.text, MonoComportamiento.score);
+            playerName.text = string.Empty;
+        }
+    }
+}
